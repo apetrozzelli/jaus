44ba3d425ceeb40e1b5bf94fe81b41d6c67c7148
Alessandro Petrozzelli <apetrozzelli@gmail.com>
2016-10-22 02:11:41 +0200

    initial import

---
+++ src/main/java/me/alessandropetrozzelli/jaus/App.java
@@ -0,0 +1,13 @@
+package me.alessandropetrozzelli.jaus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
