107830d469cf3ec0bb510f5193470e76dfbb90ee
Alessandro Petrozzelli <apetrozzelli@gmail.com>
2017-09-14 00:16:00 +0200

    Adding Vaadin web UI

---
+++ jausweb-vaadin/src/main/java/me/alessandropetrozzelli/jauswebvaadin/JauswebVaadinApplication.java
@@ -0,0 +1,41 @@
+package me.alessandropetrozzelli.jauswebvaadin;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class JauswebVaadinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JauswebVaadinApplication.class, args);
+    }
+
+    @Service
+    public static class MyService {
+        public String sayHi() {
+            return "Hello Jaus!";
+        }
+
+    }
+
+    @Theme("valo")
+    @SpringUI(path = "")
+    public static class VaadinUI extends UI {
+
+        @Autowired
+        MyService myService;
+
+        @Override
+        protected void init(VaadinRequest request) {
+            setContent(new Label(myService.sayHi()));
+        }
+
+    }
+}
